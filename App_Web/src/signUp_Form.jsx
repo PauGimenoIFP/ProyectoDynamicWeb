706b541a219ff5988e4d0433ea98411a4ae5d873
PauGimenoPrats <pau.gimeno@acceleralia.com>
2025-02-19 09:43:22 +0100

    estilos y inputs inicio

--- App_Web/src/signUp_Form.jsx
+++ App_Web/src/signUp_Form.jsx
@@ -1,4 +1,4 @@
-import './register.css'
+import './App.css'
 import logosolo from './assets/logo_dynamic_letras_blanco.png';
 
 export function SignUp_Form(){
@@ -6,8 +6,22 @@ export function SignUp_Form(){
     <main className = "main-signup-form">
       <img src={logosolo} className='logo-dynamic-form'/>
       <div className="texto-titulo-form">
-        <h1>Datos del Centro Deportivo &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Datos del Responsable</h1>
+        <h1>Datos del Centro Deportivo &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Datos del Responsable</h1>
       </div>
+      <form>
+      <div className="marco-input-form">
+        <input type='text' className="input-form">
+        </input>
+        <input type='text' className="input-form">
+        </input>
+      </div>
+      <div>
+        <input type='text' className="input-form">
+        </input>
+        <input type='text' className="input-form">
+        </input>
+      </div>
+      </form>
     </main>
   )
 }
