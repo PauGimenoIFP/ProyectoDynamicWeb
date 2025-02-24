87978c42e56e8d4e3a766c9b48b0250f8dacfcfe
andres <andresesanabria9@gmail.com>
2025-02-24 08:18:40 +0100

    nuevo fichero package payment

---
+++ App_Web/src/package_Payment.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import logosolo from './assets/logo_dynamic_letras_blanco.png';
+
+export function Package_Payment(){
+  return (
+    <main className = "main-p-p">
+      <img src={logosolo} className='logo-dynamic-s'/>
+    </main>
+  )
+}
