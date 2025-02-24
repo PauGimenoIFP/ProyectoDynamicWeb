2c2f994043237a46280aa36f0ba4b75b30be2afc
PauGimenoPrats <pau.gimeno@acceleralia.com>
2025-02-24 08:10:50 +0100

    pagina signUp Finalizada

--- App_Web/src/signUp_Form.jsx
+++ App_Web/src/signUp_Form.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import logosolo from './assets/logo_dynamic_letras_blanco.png';
+import arrow from './assets/arrow-back.png'
+import { useNavigate } from 'react-router-dom';
 
 export function SignUp_Form(){
+  const navigate = useNavigate();
+
+  const goToStart = () => {
+    navigate('/');
+  };
   return (
     <main className = "main-signup-s">
+      <div className='img-container-s'>
+      <img src={arrow} className='arrow-back-s' onClick={goToStart}/>
       <img src={logosolo} className='logo-dynamic-s'/>
+      </div>
       <form>
         <div className='grid-s'>
           <div className="marco-input-s">
@@ -13,26 +23,26 @@ export function SignUp_Form(){
             </div>
             <div className='form-s'>
               <label>Nombre del polideportivo</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="nomGym">
               </input>
               <label>Modalidad legal</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="modalidad">
               </input>
               <label>Identificador Fiscal (NIF/CIF)</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="NIF/CIF">
               </input>
               <label>Dirección completa</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="direccion">
               </input>
               <div className='grid-s'>
                 <div className='marco-CodTel-s'>
                   <label>Código Postal</label>
-                  <input type='text' className="input-s">
+                  <input type='text' className="input-s" id="codPostal">
                   </input>
                 </div>
                 <div className='marco-CodTel-s'>
                   <label>Teléfono</label>
-                  <input type='text' className="input-s">
+                  <input type='text' className="input-s" id="telefono">
                   </input>
                 </div>
               </div>
@@ -44,24 +54,28 @@ export function SignUp_Form(){
             </div>
             <div className='form-s'>
               <label>Nombre</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="nom">
               </input><br></br>
               <label>Primer apellido</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="primerApellido">
               </input>
               <label>Segundo apellido</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="segundoApellido">
               </input>
               <label>Correo electronico</label>
-              <input type='text' className="input-s">
+              <input type='text' className="input-s" id="email">
               </input>
               <label>Contraseña</label>
-              <input type='text' className="input-s">
+              <input type='password' className="input-s" id="contraseña">
               </input>
             </div>
           </div>
         </div>
       </form>
+      <div className="separador-s"></div>
+      <div className='btn-container-s'>
+        <button className='btn-save-s'>Confirmar</button>
+      </div>
     </main>
   )
 }
