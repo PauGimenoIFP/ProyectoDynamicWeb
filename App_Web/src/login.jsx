bc7f05d7688b70f9e5f1415e046ebd6d7bf8692c
andres <andresesanabria9@gmail.com>
2025-02-25 11:39:41 +0100

    mejora del boton atras

--- App_Web/src/login.jsx
+++ App_Web/src/login.jsx
@@ -1,5 +1,6 @@
 import './app.css';
 import logo from './assets/logo_dynamic_blanco.png';
+import arrow from './assets/arrow-back.png';
 import { useNavigate } from 'react-router-dom';
 
 export function Login() {
@@ -8,20 +9,30 @@ export function Login() {
   const handleNewAccount = () => {
     navigate('/Package_Payment');
   };
+
+  const handleBack = () => {
+    navigate(-1); // O la ruta que prefieras para "atrás"
+  };
+
   return(
     <main className="main-l">
+      {/* Botón de Atrás */}
+      <button className="back-button" onClick={handleBack}>
+        <img src={arrow} alt="Volver" />
+      </button>
+      
       <div className="container-login-l">
         <div className="container-logo-l">
-          <img src={logo} className="logo-dynamic-l"/>
+          <img src={logo} className="logo-dynamic-l" alt="Logo Dynamic" />
         </div>
         <div className="container-formulario-l">
           <h2 className="titulo-l">Inicio de Sesión</h2>
           <form className="formulario-l">
-            <label className="label-email-l">Correo electrónico</label>
-            <input className="email-l" placeholder=" Obligatorio" required />
-            <label className="label-contraseña-l">Contraseña</label>
-            <input className="contraseña-l" type="password" placeholder=" Obligatorio" required />
-            <button className="boton-entrar-l" type="submit"onClick={handleNewAccount}>Entrar</button>
+            <label className="label-email-l" htmlFor="email">Correo electrónico</label>
+            <input id="email" className="email-l" placeholder=" Obligatorio" required />
+            <label className="label-contraseña-l" htmlFor="contraseña">Contraseña</label>
+            <input id="contraseña" type="password" className="contraseña-l" placeholder=" Obligatorio" required />
+            <button className="boton-entrar-l" type="submit" onClick={handleNewAccount}>Entrar</button>
           </form>
         </div>
       </div>
