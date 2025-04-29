4415f82bde01d182d6724847cd231d965f5962c6
PauGimenoP <paugimenoprats@gmail.com>
2025-04-29 09:56:44 +0200

    boton de cambio de plan de suscripcion

--- App_Web/src/UserContextMenu.jsx
+++ App_Web/src/UserContextMenu.jsx
@@ -70,6 +70,54 @@ export function UserContextMenu({ x, y, isVisible, onClose, clienteId, onUserDel
             alert("No se pudo actualizar el estado de suscripción. Error: " + error.message);
         }
     };
+    const toggleSubscriptionPlan = async () => {
+        if (!clienteId || typeof clienteId !== 'string') {
+            console.error("No se proporcionó un ID de cliente válido");
+            return;
+        }
+        try {
+            const docRef = doc(db, "clientes", clienteId);
+            
+            // Obtener el estado actual
+            const docSnap = await getDoc(docRef);
+            if (!docSnap.exists()) {
+                console.error("El documento del cliente no existe");
+                return;
+            }
+            
+            // Obtener el ID del gimnasio
+            const udGym = localStorage.getItem('UdGym');
+            const gymRef = doc(db, "centrosDeportivos", udGym);
+            const gymSnap = await getDoc(gymRef);
+            if (!gymSnap.exists()) {
+                console.error("El documento del gimnasio no existe");
+                return;
+            }
+            
+            // Obtener el valor de PagoMensual
+            let PagoMensual = gymSnap.data().mensual;
+            let PagoAnual = gymSnap.data().anual;
+            let Pago;
+            // Invertir el valor actual
+            let currentPlan = docSnap.data().PlanSuscripcion;
+            if (currentPlan === 'Mensual') {
+                currentPlan = 'Anual';
+                Pago = PagoAnual;
+            } else {
+                currentPlan = 'Mensual';
+                Pago = PagoMensual;
+            }
+            
+            // Actualizar el documento
+            await updateDoc(docRef, { PlanSuscripcion: currentPlan, APagar: Pago }); // Actualiza APagar también
+            onClose();
+            if (onUserDeleted) onUserDeleted(); // Notifica al componente padre para actualizar la lista
+            
+        } catch (error) {
+            console.error("Error al actualizar el estado de suscripción:", error);
+            alert("No se pudo actualizar el estado de suscripción. Error: " + error.message);
+        }
+    };
 
     if (!isVisible) return null;
 
@@ -85,6 +133,9 @@ export function UserContextMenu({ x, y, isVisible, onClose, clienteId, onUserDel
         >
             <div className="menu-item-m-p" onClick={toggleSubscriptionStatus}>
                 Cambiar estado de pago
+            </div>
+            <div className="menu-item-m-p" onClick={toggleSubscriptionPlan}>
+                Cambiar plan de suscripción
             </div>
             <div className="menu-item-m-p" id="log-off" onClick={updateUserGymId}>
                 Eliminar usuario
